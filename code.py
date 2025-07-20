4a0e1e0b9d606a30be2b4d009f3e0fd029fd0255
Syeda Hira Fatima <syedahirafatima11@gmail.com>
2025-07-20 23:36:36 +0500

    code.py

---
+++ code.py
@@ -0,0 +1,14 @@
+import math
+
+# Basic values
+num = float(input("Enter a number: "))
+
+# Using math library functions
+print("Square root:", math.sqrt(num))
+print("Ceiling value:", math.ceil(num))
+print("Floor value:", math.floor(num))
+print("Power (number^2):", math.pow(num, 2))
+print("Logarithm base 10:", math.log10(num))
+print("Factorial (integer only):", math.factorial(int(num)))
+print("Sine of number (in radians):", math.sin(num))
+print("Pi constant:", math.pi)
